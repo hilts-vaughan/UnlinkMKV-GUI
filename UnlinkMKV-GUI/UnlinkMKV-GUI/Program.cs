91a8b882eae921e6c61af55064799002978bf4bc
Vaughan Hilts <hilts.vaughan@gmail.com>
2016-11-26 16:48:05 -0500

    UI: Removed some debugging code in the main entry point

--- UnlinkMKV-GUI/UnlinkMKV-GUI/Program.cs
+++ UnlinkMKV-GUI/UnlinkMKV-GUI/Program.cs
@@ -9,17 +9,6 @@ public class Program
     [STAThread]
 	public static void Main(String[] args)
 	{
-	    // TODO: Instead of running a WinForms application here... we can choose to just run some CLI against a hardcoded
-	    // path if we must do so
-
-	    var myDisk =
-	        "/mnt/media/AnimeToBeProcessed/[Coalgirls]_Serial_Experiments_Lain_(1520x1080_Blu-Ray_FLAC)/[Coalgirls]_Serial_Experiments_Lain_01_(1520x1080_Blu-Ray_FLAC)_[573CDDD6].mkv";
-	    var x = new MergeJob(new MergeOptions(), myDisk);
-
-	    x.PerformMerge();
-
-	    return;
-
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
         Application.Run(new FormApplication());
